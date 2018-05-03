570c483f4448b5857009c178e59d7043edeecc4d
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2018-05-03 11:34:31 +0430

    Making some changes!

--- LEARNING_WINDOWS_FORMS_APPLICATION/MainForm.cs
+++ LEARNING_WINDOWS_FORMS_APPLICATION/MainForm.cs
@@ -9,13 +9,17 @@
 
 		private void MainForm_Load(object sender, System.EventArgs e)
 		{
-			//BackColor =
-			//	System.Drawing.Color.FromArgb
-			//	(redHScrollBar.Value, greenHScrollBar.Value, blueHScrollBar.Value);
+			// Solution (1)
+			BackColor =
+				System.Drawing.Color.FromArgb
+				(red: redHScrollBar.Value, green: greenHScrollBar.Value, blue: blueHScrollBar.Value);
+			// /Solution (1)
 
-			redHScrollBar.Value = BackColor.R;
-			blueHScrollBar.Value = BackColor.B;
-			greenHScrollBar.Value = BackColor.G;
+			// Solution (2)
+			//redHScrollBar.Value = BackColor.R;
+			//blueHScrollBar.Value = BackColor.B;
+			//greenHScrollBar.Value = BackColor.G;
+			// /Solution (2)
 		}
 
 		private void HScrollBar_Scroll(object sender, System.Windows.Forms.ScrollEventArgs e)
